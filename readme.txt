3f17d8a23b432743b40195f850805e6c43b353f5
nazakatali32 <nazakatali32@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-05 15:25:33 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/wp-force-images-download/trunk@1060488 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,6 +1,7 @@
 === WP-Force Images Download === 
 Tags: image, download, force, browser force, free, pictures, post thumbnail
 Tested up to: 4.0
+Version : 1.1.0
 License: GPLv2
 
 A simple plugin that force the download of images or pictures such as jpeg,png etc.
@@ -12,13 +13,23 @@ This plugin is very useful to those who want to download post attachment or feat
 Note:The post must have featured image becuase this plugin generates download link of attached featured image of every post.
 
 <h2>HOW TO USE:</h2><br>
+1-WITH TEMPLATE TAG
 Just put the template tag in single.php file where you want to appear the download button.Put this line of code in your single.php.
 Usage:<br>
-1).  <?php wp_fid();?><br>
-2).  <?php wp_fid("Some Text");?>
+Three ways to implement..<br>
+1).  <?php wp_fid();?> simple with default settings.<br>
+2).  <?php wp_fid("Some Text");?> custom text to appear on download button<br>
+3).  <?php wp_fid("Some Text","green");?> custom text also with custom color<br>
 Second function allows you to set custom text for download button.e.g. wp_fid("Image Download");
-The default is [Download].
-If Featured Image is not set for post the download button do not appear on page.
+The default is [Download].<br>
+Note:If Featured Image is not set for post the download button do not appear on page.<br>
+
+2-WITH SHORTCODE<br>
+Now you can use shortcode.<br>
+Three ways to implement..<br>
+1).  [wpfid] simple with default settings.<br>
+2).  [wpfid title="some text"] custom text to appear on download button<br>
+3).  [wpfid title="some text" color="green"] custom text also with custom color<br>
 == Installation ==
 
 Upload the WP-Force Images Download plugin to your blog, Activate it, and you're done!
@@ -27,6 +38,14 @@ You have to put this code in single-{post} file.<br>
 <b>wp_fid();</b>
 == FAQ ==
 No questions yet
+== Changelog ==
+1.1.0
+styling and colors
+shortcode support
+
+
+1.0.0
+basic release
 #plugin-title {
   background-image: url(http://plugins.svn.wordpress.org/wp-force-images-download/assets/wp-force-images-download-772x250.jpg);
 }
--- wp_fid.php
+++ wp_fid.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: WP-Force Images Download
  * Plugin URI: https://wordpress.org/plugins/wp-force-image-download/
- * Description: A simple plugin that force the download of images or pictures such as jpeg,png etc.
- * Version: 1.0.0
+ * Description: Put wp_fid(); template tag or [wpfid] shortcode where you want to appear download button. For more details see description.
+ * Version: 1.1.0
  * Author: Nazakat ALi
  * Author URI: https://profiles.wordpress.org/nazakatali32
  * License: GPL2
@@ -24,13 +24,34 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 defined('ABSPATH') or die("No script kiddies please!");
-function wp_fid($btn_text = "Download"){
+
+function wp_fid($btn_text = "Download",$btn_color = "grey"){
 if(has_post_thumbnail()){
 $filelink = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
 $filelink = $filelink[0];
 echo "<table width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\">";
-echo "<tr><td align=\"center\"><form method=\"post\" action=\"".plugins_url('fd.php',__FILE__)."\"> <input name=\"pic_url\" type=\"hidden\" value=\"$filelink\" /><input class=\"wp_fid_btn\" type=\"submit\" title=\"". $btn_text."\" value=\"".$btn_text."\" /></form></td></tr>";
+echo "<tr><td align=\"center\"><form method=\"post\" action=\"".plugins_url('fd.php',__FILE__)."\"> <input name=\"pic_url\" type=\"hidden\" value=\"$filelink\" /><input id=\"wp_fid_button\" class=\"wpfid_button\" type=\"submit\" title=\"". $btn_text."\" value=\"".$btn_text."\" /></form></td></tr>";
 echo "</table>";
+echo "<style>#wp_fid_button{background: none repeat scroll 0% 0% ".$btn_color.";}#wp_fid_button:hover {background-color:".$btn_color.";}</style>";
+}
+};
+function wp_fid_short($atts){
+extract( shortcode_atts(
+		array(
+			'title' => 'Download',
+			'color' => 'grey'
+		), $atts )
+	);
+if(has_post_thumbnail()){
+$filelink = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+$filelink = $filelink[0];
+echo "<table width=\"100%\" border=\"0\" align=\"center\" cellpadding=\"0\" cellspacing=\"0\">";
+echo "<tr><td align=\"center\"><form method=\"post\" action=\"".plugins_url('fd.php',__FILE__)."\"> <input name=\"pic_url\" type=\"hidden\" value=\"$filelink\" /><input class=\"wpfid_button\" type=\"submit\" title=\"". $title."\" value=\"".$title."\" /></form></td></tr>";
+echo "</table>";
+echo "<style>.wpfid_button{background: none repeat scroll 0% 0% $color;}.wpfid_button:hover {background-color:$color;}</style>";
 }
 }
+echo "<style>.wpfid_button{color: white;text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);font-family: inherit;font-size: 100%;padding: 0.5em 1em;border: 0px none transparent;text-decoration: none;border-radius: 2px;display: inline-block;line-height: normal;white-space: nowrap;vertical-align: baseline;text-align: center;cursor: pointer;-moz-user-select: none;}.wpfid_button:active {position:relative;top:1px;}</style>";
+
+add_shortcode( 'wpfid' , 'wp_fid_short' );
 ?>
